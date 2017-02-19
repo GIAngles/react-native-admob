89370a77608196ed4570c76a9d0a4110dfaed087
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2017-02-19 21:18:39 +0700

    Support multiple test device IDs (backwards compatible)

--- ios/RNAdMobInterstitial.h
+++ ios/RNAdMobInterstitial.h
@@ -1,5 +1,6 @@
-#import "RCTBridgeModule.h"
-#import "RCTEventDispatcher.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTLog.h>
 @import GoogleMobileAds;
 
 @interface RNAdMobInterstitial : NSObject <RCTBridgeModule, GADInterstitialDelegate>
--- ios/RNDFPBannerView.h
+++ ios/RNDFPBannerView.h
@@ -1,4 +1,6 @@
-#import "RCTEventDispatcher.h"
+#import <React/RCTComponent.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTLog.h>
 @import GoogleMobileAds;
 
 @class RCTEventDispatcher;
@@ -7,7 +9,7 @@
 
 @property (nonatomic, copy) NSString *bannerSize;
 @property (nonatomic, copy) NSString *adUnitID;
-@property (nonatomic, copy) NSString *testDeviceID;
+@property (nonatomic, copy) NSArray *testDeviceIDs;
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 - (GADAdSize)getAdSizeFromString:(NSString *)bannerSize;
